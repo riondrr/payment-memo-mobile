0d376e67847eb8ac2f5f6ccad4488e932afd00f5
rion <mbrion04140@gmail.com>
2026-06-29 15:22:06 +0900

    feat: group payments by debit account

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "payment-notes-v4";
+const cacheName = "payment-notes-v5";
 const assets = ["./", "./index.html", "./app-config.js", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", event => {
