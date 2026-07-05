da8e6363f3c835bf5f3a59cfe6fafa7c8478534f
rion <mbrion04140@gmail.com>
2026-07-06 01:19:19 +0900

    feat: add recurring payment cycles

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "payment-notes-v10";
+const cacheName = "payment-notes-v11";
 const assets = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", event => {
