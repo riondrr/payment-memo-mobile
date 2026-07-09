458bfb7cbd783ff4d5826d53417d5f06313c046f
rion <mbrion04140@gmail.com>
2026-07-09 19:53:09 +0900

    Refine payment card layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "payment-notes-v12";
+const cacheName = "payment-notes-v13";
 const assets = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", event => {
