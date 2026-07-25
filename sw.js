e6791583f5abf406c7022e1263d00ea9cd8cc59e
rion <mbrion04140@gmail.com>
2026-07-26 00:51:21 +0900

    Improve payment and month deletion flows

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "payment-notes-v23";
+const cacheName = "payment-notes-v24";
 const assets = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", event => {
