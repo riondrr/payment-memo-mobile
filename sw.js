037cf45871c49da42bfcfd1ccb5617a560b82a6d
rion <mbrion04140@gmail.com>
2026-08-28 01:04:05 +0900

    Unify payment methods across months

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "payment-notes-v29";
+const cacheName = "payment-notes-v30";
 const assets = ["/", "/index.html", "/manifest.json", "/icon.svg"];
 
 self.addEventListener("install", event => {
