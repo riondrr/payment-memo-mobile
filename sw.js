1fd1d8ddcc844800f64bce3706b44068e38f9641
rion <mbrion04140@gmail.com>
2026-08-29 19:50:29 +0900

    Migrate payment methods to unified v13 master

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "payment-notes-v30";
+const cacheName = "payment-notes-v31";
 const assets = ["/", "/index.html", "/manifest.json", "/icon.svg"];
 
 self.addEventListener("install", event => {
